7a4a66877d00e00ec2c18f231d7c95405927dcd2
Jino Bae <im@baejino.com>
2025-04-17 07:28:23 +0900

    ⭐ Add placeholder schema

--- server/src/src/schema/index.ts
+++ server/src/src/schema/index.ts
@@ -2,17 +2,20 @@ import { makeExecutableSchema } from '@graphql-tools/schema';
 import { noteResolvers, noteTypeDefs } from './note';
 import { imageResolvers, imageTypeDefs } from './image';
 import { tagResolvers, tagTypeDefs } from './tag';
+import { placeholderResolvers, placeholderTypeDefs } from './placeholder';
 
 const schema = makeExecutableSchema({
     typeDefs: [
         noteTypeDefs,
         imageTypeDefs,
-        tagTypeDefs
+        tagTypeDefs,
+        placeholderTypeDefs
     ],
     resolvers: [
         noteResolvers,
         imageResolvers,
-        tagResolvers
+        tagResolvers,
+        placeholderResolvers
     ]
 });
 

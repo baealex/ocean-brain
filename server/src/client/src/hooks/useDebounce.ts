10bfa4087b5bb550a2db766fc0bea6e4613ae2e9
Jino Bae <im@baejino.com>
2025-05-01 14:36:11 +0900

    📦 Update template source (#2)

--- server/src/client/src/hooks/useDebounce.ts
+++ server/src/client/src/hooks/useDebounce.ts
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 const useDebounce = (delay: number) => {
     const [isMounted, setIsMounted] = useState(false);
 
-    const ref = useRef<ReturnType<typeof setTimeout>>();
+    const ref = useRef<ReturnType<typeof setTimeout>>(null);
 
     return [
         isMounted,

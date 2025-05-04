73d1dcba9ae566152cde898afdda492eabe32835
Jino Bae <im@baejino.com>
2025-05-05 00:09:57 +0900

    ⭐ Shows hints about the feature

---
+++ server/src/client/src/components/entities/Images/Images.tsx
@@ -0,0 +1,35 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { fetchImages } from '~/apis/image.api';
+import type { Image } from '~/models/image.model';
+
+interface ImagesProps {
+    searchParams: {
+        offset: number;
+        limit: number;
+    };
+    render: (data: {
+        images: Image[];
+        totalCount: number;
+    }) => React.ReactNode;
+}
+
+const Images = (props: ImagesProps) => {
+    const { data } = useSuspenseQuery({
+        queryKey: ['images', props.searchParams.offset, props.searchParams.limit],
+        async queryFn() {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            const response = await fetchImages({
+                offset: props.searchParams.offset,
+                limit: props.searchParams.limit
+            });
+            if (response.type === 'error') {
+                throw response;
+            }
+            return response.allImages;
+        }
+    });
+
+    return props.render(data);
+};
+
+export default Images;
--- server/src/client/src/pages/Search.tsx
+++ server/src/client/src/pages/Search.tsx
@@ -1,10 +1,17 @@
 import { Link, useSearchParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 
-import { Container, Highlight, Pagination } from '~/components/shared';
+import {
+    Container,
+    Empty,
+    FallbackRender,
+    Highlight,
+    Pagination
+} from '~/components/shared';
+import { Notes } from '~/components/entities';
 
-import useNotes from '~/hooks/resource/useNotes';
 import { getNoteURL } from '~/modules/url';
+import { Suspense } from 'react';
 
 export default function Search() {
     const [searchParams, setSearchParams] = useSearchParams();
@@ -13,49 +20,67 @@ export default function Search() {
     const page = Number(searchParams.get('page')) || 1;
     const query = searchParams.get('query') || '';
 
-    const { data } = useNotes({
-        query,
-        limit,
-        offset: (page - 1) * limit,
-        fields: ['content']
-    });
-
     return (
         <>
             <Helmet>
                 <title>Search "{query}" | Ocean Brain</title>
             </Helmet>
             <Container>
-                {data?.notes.map((note) => (
-                    <div key={note.id} className="mb-5">
-                        <div className="font-semibold text-lg mb-1">
-                            <Link to={getNoteURL(note.id)}>
-                                <Highlight match={query}>
-                                    {note.title}
-                                </Highlight>
-                            </Link>
-                        </div>
-                        <div className="bg-zinc-100 dark:bg-zinc-800 p-3 rounded-lg">
-                            {(JSON.parse(note.content) as object[]).filter(item => JSON.stringify(item).includes(query)).map((item) => (
-                                <Highlight match={query}>
-                                    {JSON.stringify(item)}
-                                </Highlight>
-                            ))}
-                        </div>
-                    </div>
-                ))}
-                {data?.totalCount && limit < data.totalCount && (
-                    <Pagination
-                        page={page}
-                        last={Math.ceil(data.totalCount / limit)}
-                        onChange={(page) => {
-                            setSearchParams(searchParams => {
-                                searchParams.set('page', page.toString());
-                                return searchParams;
-                            });
+                <Suspense fallback={null}>
+                    <Notes
+                        searchParams={{
+                            query,
+                            limit,
+                            offset: (page - 1) * limit,
+                            fields: ['content']
                         }}
+                        render={({ notes, totalCount }) => (
+                            <>
+                                <FallbackRender
+                                    fallback={(
+                                        <Empty
+                                            icon="👀"
+                                            title="No results found"
+                                            description="Try searching for something else."
+                                        />
+                                    )}>
+                                    {notes.length > 0 && notes.map((note) => (
+                                        <div key={note.id} className="mb-5">
+                                            <div className="font-semibold text-lg mb-1">
+                                                <Link to={getNoteURL(note.id)}>
+                                                    <Highlight match={query}>
+                                                        {note.title}
+                                                    </Highlight>
+                                                </Link>
+                                            </div>
+                                            <div className="bg-zinc-100 dark:bg-zinc-800 p-3 rounded-lg">
+                                                {(JSON.parse(note.content) as object[]).filter(item => JSON.stringify(item).includes(query)).map((item) => (
+                                                    <Highlight match={query}>
+                                                        {JSON.stringify(item)}
+                                                    </Highlight>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    ))}
+                                </FallbackRender>
+                                <FallbackRender fallback={null}>
+                                    {totalCount && limit < totalCount && (
+                                        <Pagination
+                                            page={page}
+                                            last={Math.ceil(totalCount / limit)}
+                                            onChange={(page) => {
+                                                setSearchParams(searchParams => {
+                                                    searchParams.set('page', page.toString());
+                                                    return searchParams;
+                                                });
+                                            }}
+                                        />
+                                    )}
+                                </FallbackRender>
+                            </>
+                        )}
                     />
-                )}
+                </Suspense>
             </Container>
         </>
     );

0ae15ebc31a679ecb7e3d54378cd9ad1117c4673
Jino Bae <im@baejino.com>
2025-07-11 20:36:50 +0900

    ✨ Update code style

--- server/src/src/register-aliases.ts
+++ server/src/src/register-aliases.ts
@@ -1,7 +1,6 @@
 import moduleAlias from 'module-alias';
 import path from 'path';
 
-// Register module aliases for compiled JavaScript code
 moduleAlias.addAliases({
-  '~': path.join(__dirname)
+    '~': path.join(__dirname)
 });

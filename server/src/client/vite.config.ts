12274bb5a57be1d3132de591686d9237c80e5043
Jino Bae <im@baejino.com>
2025-08-24 12:04:42 +0900

    📦 Upgrade tailwind@4

--- server/src/client/vite.config.ts
+++ server/src/client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
@@ -7,17 +8,26 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         react(),
-        svgr()
+        svgr(),
+        tailwindcss()
     ],
-    css: { preprocessorOptions: { scss: { api: 'modern' } } },
     resolve: { alias: { '~': path.resolve(__dirname, './src') } },
     build: {
         sourcemap: false,
         rollupOptions: {
             output: {
                 manualChunks: {
-                    jino: [
+                    bae: [
                         '@baejino/icon'
+                    ],
+                    core: [
+                        '@blocknote/core',
+                        '@blocknote/react',
+                        '@blocknote/mantine'
+                    ],
+                    vendor: [
+                        'react',
+                        'react-dom'
                     ]
                 }
             }
